c9e2f4234b784826b3ce6e86ee87841abacade96
DungGiaIT <dung.nguyen230208@vnuk.edu.vn>
2025-05-13 16:06:34 +0700

    Chapter 4

---
+++ nextjs-blog/pages/newWeb.js
@@ -0,0 +1,9 @@
+import Link from 'next/link';
+export default function HomePage() {
+    return (
+        <div>
+            <h1>Home Page</h1>
+            <Link href="/about">Go to About Page</Link>
+        </div>
+    );
+}
