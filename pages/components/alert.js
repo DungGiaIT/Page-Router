dd3da39a88f995efad5ef34472939b3dc7f677cd
DungGiaIT <dung.nguyen230208@vnuk.edu.vn>
2025-05-27 16:38:55 +0700

    fix bug Chapter 18

--- pages/components/alert.js
+++ pages/components/alert.js
@@ -4,10 +4,13 @@ import { clsx } from 'clsx';
 export default function Alert({ children, type }) {
   return (
     <div
-      className={clsx({
-        [styles.success]: type === 'success',
-        [styles.error]: type === 'error',
-      })}
+      className={clsx(
+        'p-4 rounded-md',
+        {
+          'bg-emerald-100 text-emerald-800': type === 'success',
+          'bg-red-100 text-red-800': type === 'error',
+        }
+      )}
     >
       {children}
     </div>
